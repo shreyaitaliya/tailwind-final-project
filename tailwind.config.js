fca5ee079fece81aaff238ca81c535a74cb854f0
shreyaitaliya <shreyaitaliya11@gmail.com>
2024-03-20 13:28:00 +0530

    all done

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+tailwind.config = {
+  content: ["*.html"],
+  theme: { 
+    screens: {
+    'sm': '640px',
+
+    'md': '768px',
+
+    'lg': '1024px',
+
+    'xl': '1140px',
+
+    '2xl': '1140px',
+
+  },
+  },
+  plugins: [],
+}
